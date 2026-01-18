6fba5d9feed841b6c63c6c7e4eae9fa243bb9104
Peter <peteryuli1204@gmail.com>
2026-01-17 16:26:09 -0800

    Added Leaflet map

---
+++ src/Map.tsx
@@ -0,0 +1,38 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+
+// Fix for default marker icon in React-Leaflet
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+delete (L.Icon.Default.prototype as any)._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+})
+
+interface MapProps {
+  center?: [number, number]
+  zoom?: number
+}
+
+export default function Map({ center = [49.2827, -123.1207], zoom = 13 }: MapProps) {
+  return (
+    <MapContainer
+      center={center}
+      zoom={zoom}
+      style={{ height: '100%', width: '100%' }}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+      />
+      <Marker position={center}>
+        <Popup>
+          A sample location
+        </Popup>
+      </Marker>
+    </MapContainer>
+  )
+}
